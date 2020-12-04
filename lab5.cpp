6437557661ca03085300fbc9049cffedfba2ae34
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-04 20:53:56 +0000

    Lab-5-Zadanie-14

--- lab5.cpp
+++ lab5.cpp
@@ -1,79 +1,39 @@
-#include "Human.hpp"
 #include <iostream>
 #include <iterator>
-#include <vector>
+#include <list>
 
 int main()
 {
-    std::vector< Human >           human_vector;
-    bool                           end = false;
-    char                           letter;
-    std::string                    imie;
-    std::vector< Human >::iterator it;
-    while (!end) {
-        std::cout << "Chcesz dodac/odjac czlowieka do/z listy? [y/n]" << std::endl;
-        std::cin >> letter;
-        if (letter == 'y') {
-            std::cout << "Podaj imie lub frazę pop" << std::endl;
-            std::cin >> imie;
-            if (imie != "pop") {
-                Human dH{std::move(imie), 18, true, true};
-                human_vector.push_back(dH);
-                // human_vector_.emplace_back(dH);
-            }
-            else {
-                human_vector.pop_back();
-            }
-            std::cout << std::endl << "Aktualna lista:" << std::endl;
+    std::list< int >           lista;
+    std::list< int >::iterator it;
 
+    lista.push_back(4);
+    lista.push_front(7);
+    lista.push_front(1);
+    lista.push_front(12);
+    lista.push_front(-3);
 
-            for (it = human_vector.begin(); it != human_vector.end(); it++)
-                std::cout << it->getName() << std::endl;
+    std::cout << "lista od przodu:" << std::endl;
+    for (it = lista.begin(); it != lista.end(); it++)
+        std::cout << *it << std::endl;
 
-
-            std::cout << std::endl;
-        }
-        if (letter == 'n') {
-            std::cout << std::endl << "Koniec" << std::endl;
-            std::cout << "human_vector size: " << human_vector.size() << std::endl;
-            std::cout << "human_vector capacity: " << human_vector.capacity() << std::endl;
-            return 0;
-        }
-    }
+    std::cout << std::endl << "lista od tylu:" << std::endl;
+    for (it = (--lista.end()); it != (--lista.begin()); it--)
+        std::cout << *it << std::endl;
 }
 
 /* Po uruchomieniu otrzymano:
-  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
-  y   
-  Podaj imie lub frazę pop
-  Adam
+  lista od przodu:
+  -3
+  12
+  1
+  7
+  4
 
-  Aktualna lista:
-  Adam
-
-  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
-  y
-  Podaj imie lub frazę pop
-  Kondzio
-
-  Aktualna lista:
-  Adam
-  Kondzio
-
-  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
-  y
-  Podaj imie lub frazę pop
-  Piotrek
-
-  Aktualna lista:
-  Adam
-  Kondzio
-  Piotrek
-
-  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
-  n
-
-  Koniec
-  human_vector size: 3
-  human_vector capacity: 4
+  lista od tylu:
+  4
+  7
+  1
+  12
+  -3
 */
