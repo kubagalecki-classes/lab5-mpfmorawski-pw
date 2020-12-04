67fdca307340ec750447a3e65414a6879c1bafbf
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-04 08:45:04 +0000

    Lab-5-Zadanie-6

--- lab5.cpp
+++ lab5.cpp
@@ -1,20 +1,80 @@
+#include "Human.hpp"
 #include <iostream>
 #include <vector>
 
 int main()
 {
-    std::vector< int > vec1(100, 42);
-    std::vector< int > vec2{100, 42};
-    std::cout << "vec1: "
-              << "\tsize: " << vec1.size() << " \tcapacity: " << vec1.capacity() << std::endl;
-    std::cout << "vec2: "
-              << "\tsize: " << vec2.size() << " \tcapacity: " << vec2.capacity() << std::endl;
-
-    std::cout << "Zawartosc vec2: " << vec2[0] << " " << vec2[1] << std::endl;
+    std::vector< Human > human_vector;
+    bool                 end = false;
+    char                 letter;
+    std::string          imie;
+    while (!end) {
+        std::cout << "Chcesz dodac/odjac czlowieka do/z listy? [y/n]" << std::endl;
+        std::cin >> letter;
+        if (letter == 'y') {
+            std::cout << "Podaj imie lub frazę pop" << std::endl;
+            std::cin >> imie;
+            if (imie != "pop") {
+                Human dH{std::move(imie), 18, true, true};
+                human_vector.push_back(dH);
+                // human_vector_.emplace_back(dH);
+            }
+            else {
+                human_vector.pop_back();
+            }
+            std::cout << std::endl << "Aktualna lista:" << std::endl;
+            for (int i = 0; i < static_cast< int >(human_vector.size()); i++)
+                std::cout << human_vector[i].getName() << std::endl;
+            std::cout << std::endl;
+        }
+        if (letter == 'n') {
+            std::cout << std::endl << "Koniec" << std::endl;
+            std::cout << "human_vector size: " << human_vector.size() << std::endl;
+            std::cout << "human_vector capacity: " << human_vector.capacity() << std::endl;
+            return 0;
+        }
+    }
 }
 
 /* Po uruchomieniu otrzymano:
-  vec1:   size: 100   capacity: 100
-  vec2:   size: 2     capacity: 2
-  Zawartosc vec2: 100 42
+  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
+  y
+  Podaj imie lub frazę pop
+  Adam
+
+  Aktualna lista:
+  Adam
+
+  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
+  y
+  Podaj imie lub frazę pop
+  Michal
+
+  Aktualna lista:
+  Adam
+  Michal
+
+  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
+  y
+  Podaj imie lub frazę pop
+  pop
+
+  Aktualna lista:
+  Adam
+
+  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
+  y
+  Podaj imie lub frazę pop
+  Piotrek
+
+  Aktualna lista:
+  Adam
+  Piotrek
+
+  Chcesz dodac/odjac czlowieka do/z listy? [y/n]
+  n
+
+  Koniec
+  human_vector size: 2
+  human_vector capacity: 2
 */
